b398df39a22abccd8f5d00d91e44aa2d665ea3e7
ALLOWAKOU Ferdinand <allowakouferdinand@gmail.com>
2024-02-26 04:04:31 +0100

    last version it was fun !

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import "../styles/Cart.css"
 
 function Cart() {
     const monsteraPrice = 8
     const [cart, updateCart] = useState(0)
     const [isOpen, setIsOpen] = useState(false)
- 
+     const total = cart.reduce(
+        (acc, plantType) => acc + plantType.amount * plantType.price,
+            0
+        )
+    useEffect(() => {
+        alert(`J'aurai ${total}€ à payer 💸`)
+    }, [total])
+
     return isOpen ? (
         <div className='lmj-cart'>
             <button onClick={() => setIsOpen(false)}>Fermer</button>
